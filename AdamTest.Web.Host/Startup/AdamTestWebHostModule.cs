bdecbf86db51c18120f9555b32a918571dc4ab11
AMelaku <51392989+AMelaku@users.noreply.github.com>
2020-06-28 21:49:18 -0500

    Add files via upload

---
+++ AdamTest.Web.Host/Startup/AdamTestWebHostModule.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using AdamTest.Configuration;
+
+namespace AdamTest.Web.Host.Startup
+{
+    [DependsOn(
+       typeof(AdamTestWebCoreModule))]
+    public class AdamTestWebHostModule: AbpModule
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public AdamTestWebHostModule(IWebHostEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(AdamTestWebHostModule).GetAssembly());
+        }
+    }
+}
